91bdd13788dee1b49441eadd4d0d0dcc640384bb
Cash <cash@cashwu.com>
2019-08-29 17:09:50 +0800

    audit.net

---
+++ testAuditNet/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading.Tasks;
+using Audit.Core;
+
+namespace testAuditNet
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Start");
+
+            var order = new Order();
+
+            using (AuditScope.Create("Order:Update", () => order, new { UserName = "cash" }))
+            {
+                Console.WriteLine("Audit");
+                order.Status = EnumStatus.Finish;
+            }
+
+            AuditScope auditScope = null;
+            try
+            {
+                auditScope = await AuditScope.CreateAsync("Test:Identity", () => order, new { user = "cash" });
+                {
+                    order.Status = EnumStatus.Start;
+                }
+            }
+            finally
+            {
+                if (auditScope != null)
+                {
+                    await auditScope.DisposeAsync();
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Order
+    {
+        public Order()
+        {
+            Id = 1;
+            Name = "name";
+            Status = EnumStatus.Init;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public EnumStatus Status { get; set; }
+    }
+
+    public enum EnumStatus
+    {
+        Init = 0,
+        Start = 1,
+        Finish = 2
+    }
+}
